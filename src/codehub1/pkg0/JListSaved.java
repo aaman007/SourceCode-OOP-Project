88647b32cc0e378e6eb1da9c52888ad0378bd90c
Amanur Rahman <aaman.sunny007@gmail.com>
2018-05-09 18:19:11 +0600

    Add files via upload

---
+++ src/codehub1/pkg0/JListSaved.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codehub1.pkg0;
+
+import javax.swing.JList;
+
+/**
+ *
+ * @author Aaman
+ */
+public interface JListSaved {
+    public static JList list1 = new JList();
+    
+}
